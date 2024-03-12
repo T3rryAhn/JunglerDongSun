4579177225aa23e20fa93d25e227651919db5cc4
Arklimits <zaqokm2@gmail.com>
2024-03-12 22:28:37 +0900

    로그인 페이지(세션)

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import pymongo
-import dotenv
 
 from flask import Flask, render_template, jsonify, request, session, url_for, redirect
 from dotenv import load_dotenv
@@ -18,51 +17,62 @@ def loginpage():
     if 'username' in session:
         return render_template("index.html", username=session.get('username'), login=True)
     else:
-        return render_template("loginpage.html", login=False)
+        if 'userid' in session:
+            return render_template("loginpage.html", userid=session.get('userid'), login=False)
+        else:
+            return render_template("loginpage.html", login=False)
 
 
-@app.route("/login", methods=["get"])
+@app.route("/login", methods=["GET"])
 def login():
     _id_ = request.args.get("id")
     _pw_ = request.args.get("pw")
+    _mem_ = request.args.get("mem")
 
-    _cursor_ = _DB_.find_one({"id": _id_, "pw": _pw_})
+    if _mem_ == 'on':
+        session['userid'] = _id_
+    else:
+        session.pop('userid')
+
+    _cursor_ = _DB_.find_one({"user_id": _id_, "user_pw": _pw_})
 
     if _cursor_:
-        session['username'] = _cursor_.get("name")
+        session['username'] = _cursor_.get("user_name")
         return redirect(url_for("loginpage"))
     else:
         return redirect(url_for("loginpage"))
+
 
 @app.route("/logout")
 def logout():
     session.pop('username')
     return redirect(url_for("loginpage"))
 
-@app.route("/checkid", methods=["get"])
+
+@app.route("/checkid", methods=["POST"])
 def checkid():
     id_confirm = request.form['id']
 
-    _cursor_ = _DB_.find({"id": id_confirm})
+    _cursor_ = _DB_.find_one({"user_id": id_confirm})
 
     if _cursor_:
-        return jsonify({'status': 'error', 'message': '이미 사용 중인 아이디 입니다.'})
+        return jsonify({'result': 'stop'})
     else:
-        return jsonify({'status': 'success', 'message': '사용 가능한 아이디 입니다.'})
+        return jsonify({'result': 'good'})
 
 
-@app.route("/signup", methods=["post"])
+@app.route("/signup", methods=["POST"])
 def signup():
     _id_ = request.form['id']
     _pw_ = request.form['pw']
     _name_ = request.form['name']
 
-    _cursor_ = _DB_.find({"id": _id_})
+    _cursor_ = _DB_.find_one({"user_id": _id_})
 
     if _cursor_:
-        return jsonify({'status': 'error', 'message': '이미 사용 중인 아이디 입니다.'})
+        return jsonify({'message': '이미 사용 중인 아이디 입니다.'})
     else:
-        _DB_.insert_one({"id": _id_, "pw": _pw_, "name": _name_, "team": "1팀"})
+        _DB_.insert_one({"user_id": _id_, "user_pw": _pw_, "user_name": _name_, "user_team": "1팀"})
         session['username'] = _name_
         return redirect(url_for("loginpage"))
 
